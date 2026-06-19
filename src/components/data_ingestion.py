873589fee6d6e32124977d9828e681ffefdbaf7f
talibehbah <bah.chernor@outlook.com>
2026-06-19 12:47:43 +1000

    Create model trainer module

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,12 @@ from src.logger import logging
 from src.components.data_transformation import DataTransformation
 
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
@@ -58,3 +64,10 @@ if __name__ == "__main__":
 
     data_transformation = DataTransformation()
     data_transformation.initiate_data_transformation(train_data, test_data)
+
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
